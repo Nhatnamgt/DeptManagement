aa1c238daa876732c300c4c38afe8acb54fd8851
nhatnamgt <namnnse182539@fpt.edu.vn>
2026-09-07 20:02:20 +0700

    supabase connect

--- update.sql
+++
@@ -1,9 +0,0 @@
-﻿-- ==========================================
--- THÊM CỘT GHI CHÚ (NẾU CHƯA TỒN TẠI)
--- ==========================================
-
-ALTER TABLE QuanLyNapRut
-ADD COLUMN IF NOT EXISTS GhiChu TEXT;
-
-ALTER TABLE ChiTietKhoanVay
-ADD COLUMN IF NOT EXISTS GhiChu TEXT;
